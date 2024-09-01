5258a5de97efc8cf90cd99bc6ae536e088621a8e
Petr Saparov <saparov.petr@gmail.com>
2024-09-01 18:29:14 +0300

    one problem fixed

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './components/app/App';
+import App from './components/App/App';
 import store from './services/store';
 
 const container = document.getElementById('root') as HTMLDivElement;
